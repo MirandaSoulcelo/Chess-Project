064c74f80b0dd4db3bee2db412b97a406a556502
Miranda Soulcelo <marcelofmiranda23@gmail.com>
2024-03-10 19:53:04 -0300

    Adicionando cor para diferenciar peças brancas de peças pretas. Mlehorando assim a impressão do método imprimir tabuleiro

--- ChessFolder/Program.cs
+++ ChessFolder/Program.cs
@@ -1,25 +1,28 @@
 ﻿using Chess_Console;
 using Tabuleiro;
 using Chess_Console.Game;
-/*try
+try
 {
 
 Board board = new Board(8,8);
 
 board.PlacePiece(new Tower(board, Color.Preta), new Position(0, 0));
 board.PlacePiece(new Tower(board, Color.Preta), new Position(1,5));
-board.PlacePiece(new King(board, Color.Preta), new Position(0,2));
+board.PlacePiece(new King(board, Color.Branca), new Position(0,2));
+board.PlacePiece(new Tower(board, Color.Branca), new Position(3,5));
+board.PlacePiece(new King(board, Color.Preta), new Position(4,2));
+
 Screen.PrintOutScreen(board);
 }
 catch(ExceptionBoard e)
 {
     Console.WriteLine(e.Message);
-}*/
+}
 
-ChessPosition pos = new ChessPosition('c', 7);
+/*ChessPosition pos = new ChessPosition('c', 7);
 
 Console.WriteLine(pos);
 
-Console.WriteLine(pos.ToPosition());
+Console.WriteLine(pos.ToPosition());*/
 
 Console.ReadKey();
--- ChessFolder/Screen.cs
+++ ChessFolder/Screen.cs
@@ -1,3 +1,5 @@
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
 using Tabuleiro;
 
 namespace Chess_Console
@@ -8,19 +10,41 @@ namespace Chess_Console
         {
             for(int i=0; i<Board.Linhas; i++)
             {
+                Console.Write( 8 - i + " ");
+               
                 for(int j=0; j<Board.Colunas; j++)
-                {   if(Board.Piece(i,j) == null)
+                {   
+                    
+                    if(Board.Piece(i,j) == null)
                 {
                     Console.Write("- ");
                    
                 }
                      else
                     {
-                        Console.Write(Board.Piece(i,j) + " ");
+                        PrintOutPiece(Board.Piece(i,j));
+                        Console.Write(" ");
                     }
 
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void PrintOutPiece(ChessPiece piece)
+        {
+            if(piece.Color == Color.Branca)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;//tipo do c# que pega a cor do sistema
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;//veja: salvei a cor do sistema em aux, para que após imprimir a peça na cor que escolhi, a cor do sistema volte para o padrão que foi salvo em aux
+
             }
         }
     }
